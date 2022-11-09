8da2120420eb74df116336c075590144664c200c
Alana <115494193+alana1789@users.noreply.github.com>
2022-11-09 15:16:36 -0300

    Iniciado

---
+++ UrnaEletronica/UrnaEletronica/Program.cs
@@ -0,0 +1,14 @@
+﻿using BLL;
+using Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Eleitor eleitor = new Eleitor("99999", "João", 2);
+        EleitorBLL eleitorBLL = new EleitorBLL();
+
+        eleitorBLL.Inserir(eleitor);
+        
+    }
+}
